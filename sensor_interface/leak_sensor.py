2dc6c3687f2ac7fed11bc2b9fc64f774f4a6265c
James-Randall-14 <randallj24@student.jhs.net>
2023-10-29 20:00:04 +0000

    leak detect on cams

--- sensor_interface/leak_sensor.py
+++ sensor_interface/leak_sensor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rclpy
+from std_srvs.srv import SetBool
 from rclpy.node import Node
 import RPi.GPIO
 
@@ -17,12 +18,23 @@ class LeakSensor(Node):
         
         self.create_timer(0.25, self.check_leaks)
 
+        # Create a client for reporting leak detection to camera viewer
+        self.leak_detect_client = self.create_client(SetBool, 'leak_detection')
+
         self.declare_parameter("leak_detection", True)
     
+
+    # See if there are any leaks. Runs 4x / second
     def check_leaks(self):
         if RPi.GPIO.input(27):
+            request = SetBool.Request()
             if self.get_parameter("leak_detection").value:
                 self.logger.fatal("WATER IN THE MEH")
+                request.data = True
+                self.leak_detect_client.call_async(request)
+            else:
+                request.data = False
+                self.leak_detect_client.call_async(request)
 
 def main(args=None):
     rclpy.init(args=args)
